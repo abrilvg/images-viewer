17e8199631b895a7771b41206de142c8e2946c37
Amira Veliz <amira.abril@gmail.com>
2021-06-20 21:57:10 -0300

    adding sass and basic styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="app">
+            <h2 as='h1'>Rover Mars</h2>
+            <video autoPlay loop muted>
+                <source src="/mars.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    );
 }
 
 export default App;
